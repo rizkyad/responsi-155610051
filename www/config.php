8df7764bed761fad29dc1714eb13538e3669f21e
root <rizkyraditya34@gmail.com>
2018-08-02 23:55:05 +0700

    project tcc

---
+++ www/config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = "localhost";
+$databaseName = "tabel";
+$databaseUsername = "root";
+$databasePassword = "test";
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
